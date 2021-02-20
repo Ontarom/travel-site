a4361ad90224b376e3ac552797f97abcb025288c
Casbin <dreadenthax@gmail.com>
2021-02-20 21:42:19 +0000

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,29 +1,33 @@
-import '../styles/styles.css'
-import 'lazysizes'
-import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader'
+import "../styles/styles.css"
+import "lazysizes"
+import ClientArea from "./modules/ClientArea"
+import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
+import StickyHeader from "./modules/StickyHeader"
 
+new ClientArea()
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-let mobileMenu = new MobileMenu();
+let mobileMenu = new MobileMenu()
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
-    el.addEventListener("click", e => {
-        e.preventDefault()
-        if (typeof modal == "undefined") {
-            import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(() => modal.openTheModal(), 20)
-            }).catch(() => console.log("There was a problem."))
-        } else {
-            modal.openTheModal()
-        }
-    })
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openTheModal(), 20)
+        })
+        .catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
 })
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
